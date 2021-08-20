8014a4e44987efe3ee6c74529d58e7b37b80cfb4
Lucia Tilnakova <lucia.tilki@gmail.com>
2021-08-20 19:08:33 +0200

    Improving the forecast readability

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,33 @@
+import React, { useState } from "react";
 import axios from "axios";
-import React from "react";
 import "./WeatherForecast.css";
-import "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  function handleSubmit(response) {
-    console.log(response);
-  }
-  console.log(props);
-  let apiKey = `e5acee71cf900fe7535600e9cd0efeca`;
-  let latitude = props.data.coords.lat;
-  let longitude = props.data.coords.lon;
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(handleSubmit);
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col text-center">
-          Thu
-          <div className="icon">
-            <img src={props.data.icon} alt="weather" width="60px"></img>
+  function handleSubmit(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col text-center">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
-          <span className="WeatherForecast-temp-max">19°</span>
-          <span className="WeatherForecast-temp-min">10°</span>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `e5acee71cf900fe7535600e9cd0efeca`;
+    let latitude = props.data.coords.lat;
+    let longitude = props.data.coords.lon;
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(handleSubmit);
+    return null;
+  }
 }
